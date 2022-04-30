f136e12cc6a5c2859be8cc577ddf959190447705
Casey Geist <tonewardbound@gmail.com>
2022-04-30 12:30:47 -0400

    card class complete

--- battle.js
+++ battle.js
@@ -1,67 +1,41 @@
-// Cards will be represented by a SUIT and a RANK, or RANK of SUIT, e.g. Jack of Diamonds
 
-// Suits Clubs, Diamonds, Hearts, Spades can be represented by... 
-// strings (the words above)
-// letters 'C' 'D' 'H' and 'S'
-// or by 0, 1, 2, 3
-
-// Ranks Ace, 2, 3, 4...10, Jack, Queen, King could be represented by...
-// 0, 1, 2, ... 11, 12
-// but 1, 2, 3, ...10, 11, 12, 13 makes more sense.
-
-
+// Cards are represented by a RANK and a SUIT, e.g. Jack of Diamonds
 class Card {
     constructor(rank, suit) {
         this.rank = rank;
         this.suit = suit;
-        
     }
 
+    // Returns RANK which is stored as a number
     getRank() {
-        return this.rank;
+        return this.rank;   
     }
-    
+
+    // Returns RANK as a string for J, Q, K, A
+    getRankAsString() {
+        switch( this.rank ) {
+            case 11:
+                return "Jack";
+            case 12:
+                return "Queen";
+            case 13:
+                return "King";
+            case 14:
+                return "Ace";
+            default:    
+                return String(this.rank);   // 2 - 10
+        }
+    }
+
+    // Returns SUIT which is stored as a string
     getSuit() {
         return this.suit;
     }
-}
-
-
-class Deck {
-    constructor() {
-        this.cards = [];
-        // assemble deck (fill cards array, preferably pre-shuffled)
-    }
-
-    shuffle() {
-
-    }
-
-    // deal entire deck to the given # of players
-    deal(numPlayers) {
-
-    }
-    // deal a starting # of cards to the given # of players 
-    deal(numPlayers, cardsPerPlayer) {
-
+    
+    // Returns a string with both RANK and SUIT
+    getRankAndSuit() {
+        return this.getRankAsString() + " of " + this.getSuit();
     }
 }
 
-class Player {
-    constructor(name) {
-        this.name = name;
-        this.cardsFaceDown = [];
 
-        // this.cardsFaceUp = [];           useful for other games
-        // this.cardsInHand = [];
-
-        this.lastCardPlayed = null;
-        this.points = 0;
-
-        // this.wonLastGame = false;        useful for "loser plays first" rule
-    }
-
-    playCard() {
-        this.lastCardPlayed = this.cardsFaceDown.pop();
-    }
-}
